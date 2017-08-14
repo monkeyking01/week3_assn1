a9197531056cf98a5fbd3c5ffa5e3e17de6175c9
monkeyking01 <30363775+monkeyking01@users.noreply.github.com>
2017-08-13 20:49:40 -0700

    Add files via upload
    Weeks 3 assignment 1

---
+++ wk3_assn1.R
@@ -0,0 +1,24 @@
+
+x <- subset(iris, Species=='virginica',select=Sepal.Length)
+mean(x[,1]
+
+new <- tapply(mtcars$hp, mtcars$cyl, mean)
+round(abs(new[3]-new[1]))
+
+
+
+makeVector <- function(x = numeric()) {
+        
+	m <- NULL
+        
+	set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setmean <- function(mean) m <<- mean
+        getmean <- function() m
+        list(set = set, get = get,
+             setmean = setmean,
+             getmean = getmean)
+}
